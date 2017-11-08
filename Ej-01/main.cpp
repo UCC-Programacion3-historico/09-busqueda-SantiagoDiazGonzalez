f592709f25204d4bd9ec51607b09dd6e0c5fb0aa
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-11-08 10:15:33 -0300

    ejercicio 1

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -18,3 +18,18 @@ void fn(int x, int v[], int ini, int fin, int pos, bool retorno) {
             ++pos;
     }
 }
+
+//El algoritmo realiza una busqueda secuencial en un arreglo, efectuada
+//por un metodo iterativo. Se busca en compara en el arreglo todos los elementos
+//hasta que se encuentra el que coincide con el elemento a buscar.
+//Si no existe en el arreglo, la variable posicion superara el tamaño del arreglo.
+
+void fr(int x, int v[], int ini, int fin, int pos){
+    pos=ini;
+    if(ini>fin)
+        return;
+    else if(v[pos] == x)
+        return;
+    else
+        fr(x,v,ini++,fin,pos);
+}
